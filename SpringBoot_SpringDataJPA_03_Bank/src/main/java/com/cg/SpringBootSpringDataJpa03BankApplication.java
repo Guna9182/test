dfd225a03d7af0181ebf9aa7737debae20d00b10
Guna <gunasekhar.m@capgemini.com>
2023-01-12 19:52:16 +0530

    Spring_Bank

---
+++ SpringBoot_SpringDataJPA_03_Bank/src/main/java/com/cg/SpringBootSpringDataJpa03BankApplication.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringDataJpa03BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataJpa03BankApplication.class, args);
+		
+		System.out.println("Employee Started");
+	}
+
+}
---
+++ SpringBoot_SpringDataJPA_03_Bank/src/test/java/com/cg/SpringBootSpringDataJpa03BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpringDataJpa03BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
